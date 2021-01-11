406db768cff9d4be87f6e8310c4689d0dc28f64c
amote <amotekades@gmail.com>
2021-01-11 14:17:22 +0000

    Initial version of gatewaysigmap generated by JHipster-6.10.5

---
+++ src/main/java/sn/ssi/sigmap/service/InvalidPasswordException.java
@@ -0,0 +1,11 @@
+package sn.ssi.sigmap.service;
+
+public class InvalidPasswordException extends RuntimeException {
+
+    private static final long serialVersionUID = 1L;
+
+    public InvalidPasswordException() {
+        super("Incorrect password");
+    }
+
+}
