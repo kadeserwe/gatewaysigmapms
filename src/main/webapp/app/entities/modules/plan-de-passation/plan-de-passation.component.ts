12fd4153a8bdfc361ed713ce8c2927411f08b9ef
JeanNoelNdiaye <64369317+JeanNoelNdiaye@users.noreply.github.com>
2021-02-18 09:58:58 +0000

    second commit 18/02/2021

---
+++ src/main/webapp/app/entities/modules/plan-de-passation/plan-de-passation.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'jhi-plan-de-passation',
+  templateUrl: './plan-de-passation.component.html',
+  styleUrls: ['./plan-de-passation.component.scss'],
+})
+export class PlanDePassationComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
